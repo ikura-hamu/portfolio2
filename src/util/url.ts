759982527b1ba4b8e392c1edabffe04364b81625
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-02-09 00:00:08 +0900

    :lipstick: Workのスタイル付けた

---
+++ src/util/url.ts
@@ -0,0 +1,13 @@
+const urlRegex =
+  /http(s)?:\/\/([a-zA-Z0-9-_]+\.)+[a-zA-Z]+(\/[a-zA-Z0-9-_]+)*(\/#[.\S]+)*/g;
+
+const replaceURL = (content: String) => {
+  return content.replaceAll(
+    urlRegex,
+    (url) => {
+      return `<a href="${url}" target="_blank">${url}</a>`;
+    }
+  );
+};
+
+export { replaceURL };

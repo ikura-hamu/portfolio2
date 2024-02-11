e1434b15cdec401c58dac783321e480b13404daf
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-02-11 17:58:53 +0900

    ブログのスタイル

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import remarkBreaks from 'remark-breaks';
+import remarkToc from 'remark-toc';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon()]
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  markdown: {
+    remarkPlugins: [remarkBreaks, [remarkToc, {heading: "目次", maxDepth: 3, tight: true, skip: '目次'}]]
+  }
 });

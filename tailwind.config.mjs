7d917d4f20403ea422d8fb3334ae3505b0c339fe
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-02-11 14:34:48 +0900

    :lipstick: 全体のスタイル

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary":"#c792ea",
+				"secondary":"#C5E478",
+				"dark": "#0B253A",
+			}
+		},
 	},
 	plugins: [],
 }

506fa509309380c6e84325b4e05088bf49915060
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-02-24 23:55:10 +0900

    :sparkles: アイコン修正

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'いくら・はむ';
-export const SITE_DESCRIPTION = 'いくらよりもめんたいこ、ハムよりもベーコン。';
+export const SITE_DESCRIPTION = 'いくらは好きだけど、ハムよりはベーコン。';
 export const SHIKI_THEME = "one-dark-pro";

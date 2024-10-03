48c97b9094c892bde208d62192d7484ae8a9c3d6
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-10-03 11:39:09 +0900

    :bug: URLを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,35 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import remarkBreaks from 'remark-breaks';
-import remarkToc from 'remark-toc';
+import remarkBreaks from "remark-breaks";
+import remarkToc from "remark-toc";
 
 import icon from "astro-icon";
-import { SHIKI_THEME } from './src/consts';
+import { SHIKI_THEME } from "./src/consts";
 
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://portfolio.ikura-hamu.work',
+  site: "https://ikura-hamu.work",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
   markdown: {
-    remarkPlugins: [remarkBreaks, [remarkToc, {
-      heading: "目次",
-      maxDepth: 3,
-      tight: true,
-      skip: '目次'
-    }]],
+    remarkPlugins: [
+      remarkBreaks,
+      [
+        remarkToc,
+        {
+          heading: "目次",
+          maxDepth: 3,
+          tight: true,
+          skip: "目次",
+        },
+      ],
+    ],
     shikiConfig: {
-      theme: SHIKI_THEME
-    }
+      theme: SHIKI_THEME,
+    },
   },
   output: "static",
   adapter: vercel({

19f2e86dcd2870dc22823ef48e67b7618dcf9a39
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-02-13 19:12:09 +0900

    :recycle: themeをglobal.cssに移動

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"primary": "#c792ea",
-				"primary-dark": "#b06fd1", // Slightly darker than primary
-				"secondary": "#C5E478",
-				"dark": "#0B253A",
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [],
+};

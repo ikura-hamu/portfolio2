4acc1e56ebd9010a7ad16d344e8bafa2cc7988b9
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-04-01 20:24:24 +0900

    :sparkles: 春休み振り返り書いた

--- src/content/blog/_schema.ts
+++ src/content/blog/_schema.ts
@@ -1,11 +1,12 @@
-import { z } from 'astro:content'
+import { z, type SchemaContext } from "astro:content";
 
-export const post = z.object({
-  title: z.string(),
-  description: z.string().optional(),
-  // Transform string to Date object
-  pubDate: z.coerce.date(),
-  updatedDate: z.coerce.date().optional(),
-  heroImage: z.string().optional(),
-  tags: z.array(z.string()).optional(),
-})
+export const post = (context: SchemaContext) =>
+  z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  });

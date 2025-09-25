f5367ce30a42596284f9ddb94f87b4f105144395
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2025-09-25 22:51:26 +0900

    :chair: Hero imageの読み込みをastroで処理して速くする

--- src/content/blog/_schema.ts
+++ src/content/blog/_schema.ts
@@ -1,12 +1,16 @@
-import { z } from "astro:content";
+import { z, type ImageFunction } from "astro:content";
 
-export const post = () =>
-  z.object({
+export const post = ({ image }: { image: ImageFunction }) => {
+  return z.object({
     title: z.string(),
     description: z.string().optional(),
     // Transform string to Date object
     pubDate: z.date(),
     updatedDate: z.date().optional(),
     heroImage: z.string().optional(),
+    heroImageContent: image()
+      .refine(() => true)
+      .optional(),
     tags: z.array(z.string()).optional(),
   });
+};

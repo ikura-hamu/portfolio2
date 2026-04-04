19c398b33af89de7dc2d287f260cce6e37b6b43e
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2026-04-04 21:32:49 +0900

    :adhesive_bandage: skillの星表記をなくす

--- src/content/skill/_schema.ts
+++ src/content/skill/_schema.ts
@@ -3,7 +3,6 @@ import { z } from "astro:content";
 export const skills = z.array(
   z.object({
     name: z.string(),
-    level: z.number(),
     description: z.string(),
     icon: z.string(),
   }),

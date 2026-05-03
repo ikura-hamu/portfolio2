214e8f769e6888d2782c08b5f348c78e76abda27
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2026-05-03 15:26:00 +0900

    :adhesive_bandage: deprecatedなzodを置き換え

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 import { skills } from "./content/skill/_schema";
 import { post } from "./content/blog/_schema";
